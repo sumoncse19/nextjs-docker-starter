c36f59d93e388eb9669a4e9b687e1908b5abc3e3
sumoncse19 <mdsumoncse19@gmail.com>
2025-05-01 03:23:55 +0600

    update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24 text-6xl font-bold">
-      <h1>Zatiq Easy</h1>
+      <h1>Next.js-React-Typescript-Docker</h1>
     </div>
   );
 }
